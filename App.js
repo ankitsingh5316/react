ed759cd4ed8a0b333aab86feed4890d8b07d94c0
Ankit Singh <109736406+ankitsingh5316@users.noreply.github.com>
2023-07-05 06:56:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import HeroSection from './components/Hero';
+
+function App() {
+  return (
+    <div className="App">
+     <Form/>
+     <HeroSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+ function Form() {
+  return (
+    <div>
+      <nav>
+        <div className='logo'>
+            <img src="project-1\public\image\logo.png"/>
+        </div>
+        <ul>
+         <li href="#">MENU</li>   
+         <li href="#">LOCATION</li>
+         <li href="#">ABOUT</li>
+         <li href="#">CONTACT</li>
+        </ul>
+        <button>Login </button>
+      </nav>
+    </div>
+  );
+}
+export default Form;
